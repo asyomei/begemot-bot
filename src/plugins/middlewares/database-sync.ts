5f25da0a56454533b07160275323af6f1a8e5688
asyomei <vabgalimov@vivaldi.net>
2024-01-24 19:39:15 +0500

    Fix database sync

--- src/plugins/middlewares/database-sync.ts
+++ src/plugins/middlewares/database-sync.ts
@@ -11,7 +11,7 @@ export default async (ctx: MyContext, next: NextFunction) => {
 				firstName: ctx.from.first_name,
 				lastName: ctx.from.last_name,
 				username: ctx.from.username,
-				messages: { increment: ctx.message && 1 },
+				messages: ctx.message && { increment: 1 },
 			},
 			create: {
 				id: ctx.from.id,
