ba0b2bac9e009fcfcbe83c30544384178a8538ee
asyomei <vabgalimov@vivaldi.net>
2024-01-27 19:53:42 +0500

    Upgrade ratelimiter

--- src/plugins/middlewares/ratelimiter.ts
+++ src/plugins/middlewares/ratelimiter.ts
@@ -1,3 +1,32 @@
 import { limit } from "@grammyjs/ratelimiter"
+import { Composer } from "grammy"
+import { redis } from "#/redis"
+import { MyContext } from "#/types/context"
 
-export default limit()
+const redisClient = {
+	incr: (key: string) => redis.incr(key),
+	pexpire: (key: string, ms: number) => redis.pExpire(key, ms).then(Number),
+}
+
+const onLimitExceeded = (ctx: MyContext) =>
+	ctx.callbackQuery && ctx.answerCallbackQuery()
+
+const comp = new Composer()
+export default comp
+
+comp.use(
+	// for users
+	limit({
+		onLimitExceeded,
+		keyGenerator: (ctx) => ctx.from && `u${ctx.from.id}`,
+		storageClient: redisClient,
+	}),
+	// for chats
+	limit({
+		limit: 5,
+		timeFrame: 1500,
+		onLimitExceeded,
+		keyGenerator: (ctx) => ctx.chat && `c${ctx.chat.id}`,
+		storageClient: redisClient,
+	}),
+)
