05a26cf059473d1786a6c2eaf53fee9de7ce79d5
asyomei <vabgalimov@vivaldi.net>
2024-02-02 17:47:41 +0500

    Ignore message is not modefied error

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,13 @@
-import { Bot } from "grammy"
+import { Bot, GrammyError } from "grammy"
 import { env } from "./env"
 import { MyContext } from "./types/context"
 
 export const bot = new Bot<MyContext>(env.BOT_TOKEN)
 
-bot.catch(({ stack }) => {
+bot.catch(({ error, stack }) => {
+	if (error instanceof GrammyError) {
+		if (error.description.includes("message is not modified")) return
+	}
+
 	console.error(stack)
 })
