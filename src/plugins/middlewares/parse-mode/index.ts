32621636f032ad5c274d1b87022593f9c663b72d
asyomei <vabgalimov@vivaldi.net>
2024-02-05 16:30:21 +0500

    Use @grammyjs/parse-mode

--- src/plugins/middlewares/parse-mode/index.ts
+++ src/plugins/middlewares/parse-mode/index.ts
@@ -1,15 +1,10 @@
-import { Context, NextFunction, Transformer } from "grammy"
+import { parseMode } from "@grammyjs/parse-mode"
+import { Context, NextFunction } from "grammy"
+
+const transformer = parseMode("HTML")
 
 export default async (ctx: Context, next: NextFunction) => {
 	ctx.api.config.use(transformer)
 
 	await next()
-}
-
-const transformer: Transformer = async (prev, method, payload, signal) => {
-	if (method.startsWith("send") && method !== "sendChatAction") {
-		payload = { parse_mode: "HTML", ...payload }
-	}
-
-	return await prev(method, payload, signal)
 }
